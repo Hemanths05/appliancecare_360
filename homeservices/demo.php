b6d3e23ecb36cc5cba99404bd8b68041ec614e62
Mathan <mathans2609@gmail.com>
2023-11-06 19:02:24 +0530

    first_commit

---
+++ homeservices/demo.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once "./include/header.php";
+$cities = ["coimbatore"];
+
+?>
+<div class="form-group col-5">
+            <label for="">City</label>
+            <select class="form-control" name="city" id="city">
+                <option value="none">-- Select City --</option>
+                <?php foreach ($cities as $city) : ?>
+                <option value="<?= $city ?>"> <?= $city ?>
+                </option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
+        <script src="js/jquery.js"></script>
+<script>
+    $(function() {
+        $("#search").click(function() {
+            var city = $("#city").val();
+            var profession = $("#profession").val();
+
+            if (city == "none" || profession == "none") {
+                alert("Don't leave fields empty!");
+                tbody = "<tr><td colspan='5'>please </td></tr>";
+            } else {
+                $.post('scripts/searchproviders.php', {
+                    city: city,
+                    profession: profession
+                }, function(res) {
+                    var providers = JSON.parse(res);
+                    var tbody = "";
+
+                   
+                    $("#providers tbody").html(tbody);
+                });
+            }
+        });
+    });
+</script>
+
+<?php include_once "./include/footer.php";
